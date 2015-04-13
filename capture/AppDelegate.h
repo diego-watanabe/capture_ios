693221d58ba243c2992681147dc06fb1fe222f9c
Diego Watanabe <diego@inceptures.com>
2015-04-13 00:45:04 -0400

    Initial Commit

---
+++ capture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  capture
+//
+//  Created by Diego Watanabe on 4/13/15.
+//  Copyright (c) 2015 blorgod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ capture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  capture
+//
+//  Created by Diego Watanabe on 4/13/15.
+//  Copyright (c) 2015 blorgod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
