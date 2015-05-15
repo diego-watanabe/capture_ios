92f2a3cff65ba7fb4d857a69b6cf82fe0c5bfa46
diego-watanabe <dwatanabe314@gmail.com>
2015-05-15 14:53:22 -0400

    commit before testing something

--- capture/ResultsViewController.h
+++ capture/ResultsViewController.h
@@ -46,7 +46,6 @@
 }
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UITextView *longtext;
-@property (weak, nonatomic) IBOutlet UILabel *label11;
 
 @property (weak, nonatomic) IBOutlet UILabel *label1;
 @property (weak, nonatomic) IBOutlet UILabel *label2;
@@ -58,9 +57,11 @@
 @property (weak, nonatomic) IBOutlet UILabel *label8;
 @property (weak, nonatomic) IBOutlet UILabel *label9;
 @property (weak, nonatomic) IBOutlet UILabel *label10;
+@property (weak, nonatomic) IBOutlet UILabel *label11;
 @property (weak, nonatomic) IBOutlet UILabel *result1;
 @property (weak, nonatomic) IBOutlet UILabel *result2;
 @property (weak, nonatomic) IBOutlet UILabel *result3;
+@property (weak, nonatomic) IBOutlet UILabel *detailsTitleLabel;
 
 @property (weak, nonatomic) IBOutlet UILabel *result4;
 @property (weak, nonatomic) IBOutlet UILabel *result5;
@@ -70,21 +71,23 @@
 @property (weak, nonatomic) IBOutlet UILabel *result8;
 @property (weak, nonatomic) IBOutlet UILabel *result9;
 @property (weak, nonatomic) IBOutlet UILabel *result10;
-@property (weak, nonatomic) IBOutlet UIButton *recoveryButton;
 
-- (IBAction)recoverImage:(id)sender;
-@property (weak, nonatomic) IBOutlet UILabel *detailsTitleLabel;
+@property (weak, nonatomic) IBOutlet UIButton *recoveryButton;
 @property (weak, nonatomic) IBOutlet UIButton *userButton;
-- (IBAction)userButtonTouchUpInside:(id)sender;
-@property (weak, nonatomic) IBOutlet UIView *displayDetails;
 @property (weak, nonatomic) IBOutlet UIButton *networkButton;
 @property (weak, nonatomic) IBOutlet UIButton *captureButton;
+@property (weak, nonatomic) IBOutlet UIButton *storeButton;
+
+@property (weak, nonatomic) IBOutlet UIView *imageButton;
+@property (weak, nonatomic) IBOutlet UIView *displayDetails;
+
+@property (nonatomic, retain) NSMutableDictionary *quotes;
+
 - (IBAction)captureButtonTouchUpInside:(id)sender;
 - (IBAction)storeButtonTouchUpInside:(id)sender;
-@property (weak, nonatomic) IBOutlet UIButton *storeButton;
-@property (nonatomic, retain) NSMutableDictionary *quotes;
-@property (weak, nonatomic) IBOutlet UIView *imageButton;
 - (IBAction)imageButtonTouchUpInside:(id)sender;
 - (IBAction)networkButtonTouchUpInside:(id)sender;
 - (IBAction)exitDetailsTouchUpInside:(id)sender;
+- (IBAction)recoverImage:(id)sender;
+- (IBAction)userButtonTouchUpInside:(id)sender;
 @end
